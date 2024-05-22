392d27ea4f8367040e7b56a5b396fe7daa142787
ermeson119 <ermesonbalbinot119@gmail.com>
2024-05-22 18:50:25 -0300

    adicionando rota para link, whatsApp, instagram, linkedin

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def home(request):
     return render(request, 'todos/home.html')
 
 def projetos(request):
     return render(request, 'todos/projetos.html')
+
+def linkedin(request):
+    linkedin = 'https://www.linkedin.com/in/ermeson-balbinot/'
+    return redirect(linkedin)
+
+def instagram(request):
+    instagram = 'https://www.instagram.com/ermesonbalbinot/'
+    return redirect(instagram)
+
+def whatsapp(request):
+    whatsapp = 'https://wa.me/qr/OKMH27RO567BB1'
+    return redirect(whatsapp)
+
